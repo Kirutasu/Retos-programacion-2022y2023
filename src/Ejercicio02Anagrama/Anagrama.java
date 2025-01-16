73aadb09c08a3694a0c8436a5a50b14e34d09a60
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-01-16 21:13:23 +0100

    main principal pero sin uso, cada ejercicio tiene su ejecutable, creado paquete ejercicio 2

--- src/Ejercicio02Anagrama/Anagrama.java
+++ src/Ejercicio02Anagrama/Anagrama.java
@@ -1,4 +1,9 @@
 package Ejercicio02Anagrama;
 
 public class Anagrama {
+
+    public static void main(String[] args) {
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,5 @@
-package Ejercicio01Fizzbuzz;
-
-public class Fizzbuzz {
+public class Main {
 
     public static void main(String[] args) {
-        /*
-         * Escribe un programa que muestre por consola (con un print) los
-         * números de 1 a 100 (ambos incluidos y con un salto de línea entre
-         * cada impresión), sustituyendo los siguientes:
-         * - Múltiplos de 3 por la palabra "fizz".
-         * - Múltiplos de 5 por la palabra "buzz".
-         * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
-         */
-
-        /* Desglose  de Tareas:
-         * -Mostrar TODOS los numeros de 1 a 100
-         * -Mostrarlos CON salto linea cada impresion
-         * -SUSTITUIR:
-         * -IF multiplo de 3 >> fizz
-         * -IF multiplo 5 >> buzz
-         * -IF multiplo 3 y 5 >> fizzbuzz
-         * -PrintLn
-         */
-
-        int numeros = 1;
-        String tres = "fizz";
-        String cinco = "buzz";
-        String trescinco = "fizzbuzz";
-
-
-        for (int i = 0; i < 100; i++) {
-            if ((numeros %3 == 0) && (numeros %5 == 0)) {
-                System.out.println(trescinco);
-            } else if (numeros %3 == 0){
-                System.out.println(tres);
-            } else if (numeros %5 == 0) {
-                System.out.println(cinco);
-            }else {
-                System.out.println(numeros);
             }
-            numeros++;
-        }
-
-    }
 }
